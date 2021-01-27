93eab4a2d617ee7e5c073a24bf50016b0a7c513f
timmyliang <timmyliang@tencent.com>
2021-01-27 16:30:52 +0800

    add ascii fbx export

--- fbx_exporter/__init__.py
+++ fbx_exporter/__init__.py
@@ -7,21 +7,116 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-__author__ = 'timmyliang'
-__email__ = '820472580@qq.com'
-__date__ = '2021-01-26 20:44:17'
+__author__ = "timmyliang"
+__email__ = "820472580@qq.com"
+__date__ = "2021-01-26 20:44:17"
 
 
+import os
+import json
 import struct
-from collections import defaultdict
+from textwrap import dedent
+from collections import defaultdict, OrderedDict
 
+FBX_ASCII_TEMPLETE = """
+    ; FBX 7.3.0 project file
+    ; ----------------------------------------------------
+
+    FBXHeaderExtension:  {
+        FBXHeaderVersion: 1003
+        FBXVersion: 7300
+        CreationTimeStamp:  {
+            Version: 1000
+            Year: 2021
+            Month: 1
+            Day: 26
+            Hour: 21
+            Minute: 4
+            Second: 59
+            Millisecond: 682
+        }
+    }
+
+
+    ; Object definitions
+    ;------------------------------------------------------------------
+
+    Definitions:  {
+
+        ObjectType: "Geometry" {
+            Count: 1
+            PropertyTemplate: "FbxMesh" {
+                Properties70:  {
+                    P: "Primary Visibility", "bool", "", "",1
+                }
+            }
+        }
+
+        ObjectType: "Model" {
+            Count: 1
+            PropertyTemplate: "FbxNode" {
+                Properties70:  {
+                    P: "Visibility", "Visibility", "", "A",1
+                }
+            }
+        }
+    }
+
+    ; Object properties
+    ;------------------------------------------------------------------
+
+    Objects:  {
+        Geometry: 2035541511296, "Geometry::", "Mesh" {
+            Vertices: *%(vertices_num)s {
+                a: %(vertices)s
+            } 
+            PolygonVertexIndex: *%(polygons_num)s {
+                a: %(polygons)s
+            } 
+            GeometryVersion: 124
+            %(LayerElementNormal)s
+            %(LayerElementTangent)s
+            %(LayerElementColor)s
+            %(LayerElementUV)s
+            Layer: 0 {
+                Version: 100
+                %(LayerElementNormalInsert)s
+                %(LayerElementTangentInsert)s
+                %(LayerElementColorInsert)s
+                %(LayerElementUVInsert)s
+                
+            }
+        }
+        Model: 2035615390896, "Model::%(model_name)s", "Mesh" {
+            Properties70:  {
+                P: "DefaultAttributeIndex", "int", "Integer", "",0
+            }
+        }
+    }
+
+    ; Object connections
+    ;------------------------------------------------------------------
+
+    Connections:  {
+        
+        ;Model::pCube1, Model::RootNode
+        C: "OO",2035615390896,0
+        
+        ;Geometry::, Model::pCube1
+        C: "OO",2035541511296,2035615390896
+
+    }
+
+    """
 
 rd = renderdoc
 manager = pyrenderdoc.Extensions()
 
+
 class MeshData(rd.MeshFormat):
     indexOffset = 0
     name = ""
+
 
 # Unpack a tuple of the given format, from the data
 def unpackData(fmt, data):
@@ -31,9 +126,9 @@ def unpackData(fmt, data):
 
     formatChars = {}
     #                                 012345678
-    formatChars[rd.CompType.UInt]  = "xBHxIxxxL"
-    formatChars[rd.CompType.SInt]  = "xbhxixxxl"
-    formatChars[rd.CompType.Float] = "xxexfxxxd" # only 2, 4 and 8 are valid
+    formatChars[rd.CompType.UInt] = "xBHxIxxxL"
+    formatChars[rd.CompType.SInt] = "xbhxixxxl"
+    formatChars[rd.CompType.Float] = "xxexfxxxd"  # only 2, 4 and 8 are valid
 
     # These types have identical decodes, but we might post-process them
     formatChars[rd.CompType.UNorm] = formatChars[rd.CompType.UInt]
@@ -53,15 +148,18 @@ def unpackData(fmt, data):
         value = tuple(float(i) / divisor for i in value)
     elif fmt.compType == rd.CompType.SNorm:
         maxNeg = -float(2 ** (fmt.compByteWidth * 8)) / 2
-        divisor = float(-(maxNeg-1))
-        value = tuple((float(i) if (i == maxNeg) else (float(i) / divisor)) for i in value)
+        divisor = float(-(maxNeg - 1))
+        value = tuple(
+            (float(i) if (i == maxNeg) else (float(i) / divisor)) for i in value
+        )
 
     # If the format is BGRA, swap the two components
     if fmt.BGRAOrder():
         value = tuple(value[i] for i in [2, 1, 0, 3])
 
     return value
-    
+
+
 def getIndices(controller, mesh):
     # Get the character for the width of index
     indexFormat = "B"
@@ -88,7 +186,16 @@ def getIndices(controller, mesh):
         # With no index buffer, just generate a range
         return tuple(range(mesh.numIndices))
 
-def unpack(controller,attr,idx):
+
+def findChannel(vertexData, keywords):
+    for channel in vertexData:
+        for keyword in keywords:
+            if keyword in channel:
+                return channel
+    return None
+
+
+def unpack(controller, attr, idx):
     # This is the data we're reading from. This would be good to cache instead of
     # re-fetching for every attribute for every index
     offset = attr.vertexByteOffset + attr.vertexByteStride * idx
@@ -96,6 +203,7 @@ def unpack(controller,attr,idx):
 
     # Get the value from the data
     return unpackData(attr.format, data)
+
 
 def test(controller):
     state = controller.GetPipelineState()
@@ -108,14 +216,17 @@ def test(controller):
     # NOTE current draw draw
     draw = pyrenderdoc.CurSelectedDrawcall()
     if not draw:
-        manager.ErrorDialog('Please pick a valid draw call in the Event Browser.','Error')
+        manager.ErrorDialog(
+            "Please pick a valid draw call in the Event Browser.", "Error"
+        )
         return
-    
+
     meshInputs = []
     for attr in attrs:
         # We don't handle instance attributes
         if attr.perInstance:
-            raise RuntimeError("Instanced properties are not supported!")
+            manager.ErrorDialog("Instanced properties are not supported!", "Error")
+            return
 
         meshInput = MeshData()
         meshInput.indexResourceId = ib.resourceId
@@ -144,14 +255,185 @@ def test(controller):
 
     indices = getIndices(controller, meshInputs[0])
     if not indices:
-        manager.ErrorDialog('Current Draw Call lack of Vertex. ','Error')
+        manager.ErrorDialog("Current Draw Call lack of Vertex. ", "Error")
         return
-    
+
+    save_path = manager.SaveFileName("Save FBX File", "", "*.fbx")
+    if not save_path:
+        return
+
+    save_name = os.path.basename(os.path.splitext(save_path)[0])
+
     # We'll decode the first three indices making up a triangle
-    for i,idx in enumerate(indices):
-        mesh_data = {attr.name:unpack(controller,attr,idx) for attr in meshInputs}
-        print(mesh_data)
-        
+    idx_dict = defaultdict(list)
+    value_dict = defaultdict(list)
+    vertex_data = defaultdict(OrderedDict)
+    for i, idx in enumerate(indices):
+        for attr in meshInputs:
+            value = unpack(controller, attr, idx)
+            idx_dict[attr.name].append(idx)
+            value_dict[attr.name].append(value)
+            if idx not in vertex_data[attr.name]:
+                vertex_data[attr.name][idx] = value
+
+    # print(json.dumps(vertex_data))
+
+    polygons = [idx for idx in idx_dict["POSITION"]]
+    min_poly = min(polygons)
+    idx_list = [str(idx - min_poly) for idx in idx_dict["POSITION"]]
+
+    ARGS = {"model_name": save_name}
+    vertices = [str(v) for values in vertex_data["POSITION"].values() for v in values]
+    ARGS["vertices"] = ",".join(vertices)
+    ARGS["vertices_num"] = len(vertices)
+
+    # NOTE https://www.codenong.com/cs105411312/
+    polygons = [
+        str(idx - min_poly) if i % 3 else str(-(idx - min_poly + 1))
+        for i, idx in enumerate(idx_dict["POSITION"], 1)
+    ]
+    ARGS["polygons"] = ",".join(polygons)
+    ARGS["polygons_num"] = len(polygons)
+
+    LayerElementNormal = ""
+    LayerElementNormalInsert = ""
+    has_normal = vertex_data.get("NORMAL")
+    if has_normal:
+        normals = [str(v) for values in vertex_data["NORMAL"].values() for v in values]
+
+        LayerElementNormal = """
+            LayerElementNormal: 0 {
+                Version: 101
+                Name: ""
+                MappingInformationType: "ByPolygonVertex"
+                ReferenceInformationType: "Direct"
+                Normals: *%(normals_num)s {
+                    a: %(normals)s
+                } 
+            }
+        """ % {
+            "normals": ",".join(normals),
+            "normals_num": len(normals),
+        }
+        LayerElementNormalInsert = """
+            LayerElement:  {
+                    Type: "LayerElementNormal"
+                TypedIndex: 0
+            }
+        """
+
+    LayerElementTangent = ""
+    LayerElementTangentInsert = ""
+    has_tangent = vertex_data.get("TANGENT")
+    if has_tangent:
+        tangents = [str(v) for values in value_dict["TANGENT"] for v in values]
+        LayerElementTangent = """
+            LayerElementTangent: 0 {
+                Version: 101
+                Name: "map1"
+                MappingInformationType: "ByPolygonVertex"
+                ReferenceInformationType: "Direct"
+                Tangents: *%(tangents_num)s {
+                    a: %(tangents)s
+                } 
+            }
+        """ % {
+            "tangents": ",".join(tangents),
+            "tangents_num": len(tangents),
+        }
+
+        LayerElementTangentInsert = """
+                LayerElement:  {
+                    Type: "LayerElementTangent"
+                    TypedIndex: 0
+                }
+        """
+
+    LayerElementColor = ""
+    LayerElementColorInsert = ""
+    has_color = vertex_data.get("COLOR")
+    if has_color:
+        colors = [str(v) for values in value_dict["COLOR"] for v in values]
+
+        LayerElementColor = """
+            LayerElementColor: 0 {
+                Version: 101
+                Name: "colorSet1"
+                MappingInformationType: "ByPolygonVertex"
+                ReferenceInformationType: "IndexToDirect"
+                Colors: *%(colors_num)s {
+                    a: %(colors)s
+                } 
+                ColorIndex: *%(colors_indices_num)s {
+                    a: %(colors_indices)s
+                } 
+            }
+        """ % {
+            "colors": ",".join(colors),
+            "colors_num": len(colors),
+            "colors_indices": ",".join(idx_list),
+            "colors_indices_num": len(idx_list),
+        }
+        LayerElementColorInsert = """
+            LayerElement:  {
+                Type: "LayerElementColor"
+                TypedIndex: 0
+            }
+        """
+
+    LayerElementUV = ""
+    LayerElementUVInsert = ""
+    has_uv = vertex_data.get("TEXCOORD0")
+    if has_uv:
+        uvs = [str(v) for values in vertex_data["TEXCOORD0"].values() for v in values]
+
+        LayerElementUV = """
+            LayerElementUV: 0 {
+                Version: 101
+                Name: "map1"
+                MappingInformationType: "ByPolygonVertex"
+                ReferenceInformationType: "IndexToDirect"
+                UV: *%(uvs_num)s {
+                    a: %(uvs)s
+                } 
+                UVIndex: *%(uvs_indices_num)s {
+                    a: %(uvs_indices)s
+                } 
+            }
+        """ % {
+            "uvs": ",".join(uvs),
+            "uvs_num": len(uvs),
+            "uvs_indices": ",".join(idx_list),
+            "uvs_indices_num": len(idx_list),
+        }
+
+        LayerElementUVInsert = """
+            LayerElement:  {
+                Type: "LayerElementUV"
+                TypedIndex: 0
+            }
+        """
+
+    ARGS.update(
+        {
+            "LayerElementNormal": LayerElementNormal,
+            "LayerElementNormalInsert": LayerElementNormalInsert,
+            "LayerElementTangent": LayerElementTangent,
+            "LayerElementTangentInsert": LayerElementTangentInsert,
+            "LayerElementColor": LayerElementColor,
+            "LayerElementColorInsert": LayerElementColorInsert,
+            "LayerElementUV": LayerElementUV,
+            "LayerElementUVInsert": LayerElementUVInsert
+        }
+    )
+
+    fbx = FBX_ASCII_TEMPLETE % ARGS
+
+    with open(save_path, "w") as f:
+        f.write(dedent(fbx).strip())
+
+    # print(json.dumps(data))
+    manager.MessageDialog("FBX Ouput Sucessfully", "Congradualtion!~")
+
+
 pyrenderdoc.Replay().BlockInvoke(test)
-
-
